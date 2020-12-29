018bf8fee81d50a9a186a08ccbc79fc62ffb6aff
shinyay <shinya.com@gmail.com>
2020-12-29 22:50:31 +0900

    Add EnableCaching annotation to enable caching feature

--- src/main/kotlin/com/google/shinyay/config/CacheConfig.kt
+++ src/main/kotlin/com/google/shinyay/config/CacheConfig.kt
@@ -1,7 +1,9 @@
 package com.google.shinyay.config
 
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@EnableCaching
 class CacheConfig {
 }
