1a3497aebdd973fc2227bebb7e474435cd909cb6
shinyay <shinya.com@gmail.com>
2020-12-29 22:23:36 +0900

    Initial Commit

---
+++ src/main/kotlin/com/google/shinyay/SpringCloudGcpMemorystoreGsApplication.kt
@@ -0,0 +1,11 @@
+package com.google.shinyay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudGcpMemorystoreGsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringCloudGcpMemorystoreGsApplication>(*args)
+}
