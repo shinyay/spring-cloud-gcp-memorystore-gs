fd3486a38317df82d6e4843395bc791e4df580ca
shinyay <shinya.com@gmail.com>
2021-01-04 10:36:59 +0900

    Add BookRepository injection

--- src/main/kotlin/com/google/shinyay/controller/BookController.kt
+++ src/main/kotlin/com/google/shinyay/controller/BookController.kt
@@ -1,7 +1,8 @@
 package com.google.shinyay.controller
 
+import com.google.shinyay.repository.BookRepository
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class BookController() {
+class BookController(val repository: BookRepository) {
 }
