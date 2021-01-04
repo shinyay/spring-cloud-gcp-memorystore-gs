cb20005df4e6bd64ed3bcd8d4ce516c74e74f20b
shinyay <shinya.com@gmail.com>
2021-01-04 11:18:26 +0900

    Add getFunction by ISBN

--- src/main/kotlin/com/google/shinyay/controller/BookController.kt
+++ src/main/kotlin/com/google/shinyay/controller/BookController.kt
@@ -1,8 +1,18 @@
 package com.google.shinyay.controller
 
+import com.google.shinyay.entity.Book
 import com.google.shinyay.repository.BookRepository
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
-@RestController
+@RestController("/api/v1")
 class BookController(val repository: BookRepository) {
+
+    @GetMapping("/books/{isbn}")
+    fun getBook(@PathVariable isbn: String): Optional<Book> {
+        return repository.findById(isbn)
+    }
 }
