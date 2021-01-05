79b7e6aed762cc50c38781c2a3d159295efe089c
shinyay <shinya.com@gmail.com>
2021-01-05 11:19:13 +0900

    Add return using ResponseEntity

--- src/main/kotlin/com/google/shinyay/controller/BookController.kt
+++ src/main/kotlin/com/google/shinyay/controller/BookController.kt
@@ -2,18 +2,20 @@ package com.google.shinyay.controller
 
 import com.google.shinyay.entity.Book
 import com.google.shinyay.repository.BookRepository
+import com.google.shinyay.service.BookService
 import org.springframework.cache.annotation.Cacheable
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
 @RestController("/api/v1")
-class BookController(val repository: BookRepository) {
+class BookController(val service: BookService) {
 
-    @Cacheable(value = ["books"], key = "isbn")
     @GetMapping("/books/{isbn}")
-    fun getBook(@PathVariable isbn: String): Optional<Book> {
-        return repository.findById(isbn)
+    fun getBook(@PathVariable isbn: String): ResponseEntity<Book> {
+        return ResponseEntity<Book>(service.getBook(isbn), HttpStatus.OK)
     }
 }
