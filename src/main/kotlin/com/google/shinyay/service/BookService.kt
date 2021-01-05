19225e7c4490ddbafb38e3e0be20706fd1e000ba
shinyay <shinya.com@gmail.com>
2021-01-05 09:48:38 +0900

    Add cacheable data function

--- src/main/kotlin/com/google/shinyay/service/BookService.kt
+++ src/main/kotlin/com/google/shinyay/service/BookService.kt
@@ -1,7 +1,16 @@
 package com.google.shinyay.service
 
+import com.google.shinyay.entity.Book
+import com.google.shinyay.repository.BookRepository
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
-class BookService {
+class BookService(val repository: BookRepository) {
+
+    @Cacheable(value = ["books"], key = "isbn")
+    fun getBook(isbn: String): Optional<Book> {
+        return repository.findById(isbn)
+    }
 }
