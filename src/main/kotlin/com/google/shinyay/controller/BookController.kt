9768f1a6a5c63d33fc59ab8e844552be4708ad31
shinyay <shinya.com@gmail.com>
2021-01-06 15:22:27 +0900

    Modify URL with RequestMapping

--- src/main/kotlin/com/google/shinyay/controller/BookController.kt
+++ src/main/kotlin/com/google/shinyay/controller/BookController.kt
@@ -9,7 +9,8 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
-@RestController("/api/v1")
+@RestController
+@RequestMapping("/api/v1")
 class BookController(val service: BookService) {
 
     @GetMapping("/books")
