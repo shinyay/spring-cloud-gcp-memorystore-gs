53756134745a30d59c33dde317b54979af7a77f8
shinyay <shinya.com@gmail.com>
2021-01-06 11:37:59 +0900

    Add create RedisTemplate

--- src/main/kotlin/com/google/shinyay/config/RedisConfig.kt
+++ src/main/kotlin/com/google/shinyay/config/RedisConfig.kt
@@ -3,10 +3,23 @@ package com.google.shinyay.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.GenericToStringSerializer
+
+
+
 
 @Configuration
 class RedisConfig {
 
     @Bean
     fun jedisConnectionFactory(): JedisConnectionFactory = JedisConnectionFactory()
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, Any>? {
+        return RedisTemplate<String, Any>().apply {
+            setConnectionFactory(jedisConnectionFactory())
+            valueSerializer = GenericToStringSerializer(Any::class.java)
+        }
+    }
 }
