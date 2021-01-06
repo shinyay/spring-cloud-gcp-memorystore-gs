6e0f3eea9c368515215ad57a966c1a80675d95c5
shinyay <shinya.com@gmail.com>
2021-01-06 15:20:56 +0900

    Add Serializable interface

--- src/main/kotlin/com/google/shinyay/entity/Book.kt
+++ src/main/kotlin/com/google/shinyay/entity/Book.kt
@@ -2,7 +2,8 @@ package com.google.shinyay.entity
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
+import java.io.Serializable
 
 @RedisHash("book")
 data class Book(@Id val isbn: String,
-                var title: String)
+                var title: String) : Serializable
