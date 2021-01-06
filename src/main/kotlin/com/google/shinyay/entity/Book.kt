9fa531afc29a3316700193be6a5f8ed476fcf142
shinyay <shinya.com@gmail.com>
2021-01-06 13:27:08 +0900

    Add RedisHash Annotation

--- src/main/kotlin/com/google/shinyay/entity/Book.kt
+++ src/main/kotlin/com/google/shinyay/entity/Book.kt
@@ -3,5 +3,6 @@ package com.google.shinyay.entity
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 
+@RedisHash("book")
 data class Book(@Id val isbn: String,
                 var title: String)
