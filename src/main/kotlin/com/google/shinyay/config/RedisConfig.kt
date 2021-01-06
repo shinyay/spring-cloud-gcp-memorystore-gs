daf5e0974df184678edddb6d01ce67b982b823b2
shinyay <shinya.com@gmail.com>
2021-01-06 11:27:50 +0900

    Add connection factory for Redis

--- src/main/kotlin/com/google/shinyay/config/RedisConfig.kt
+++ src/main/kotlin/com/google/shinyay/config/RedisConfig.kt
@@ -1,4 +1,12 @@
 package com.google.shinyay.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
+
+@Configuration
 class RedisConfig {
+
+    @Bean
+    fun jedisConnectionFactory(): JedisConnectionFactory = JedisConnectionFactory()
 }
