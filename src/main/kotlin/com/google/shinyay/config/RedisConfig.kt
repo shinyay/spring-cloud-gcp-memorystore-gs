f8e4e83dc8b6091ba28cd659472dc44c13c40bef
shinyay <shinya.com@gmail.com>
2021-01-06 11:45:21 +0900

    Add Specify component scan

--- src/main/kotlin/com/google/shinyay/config/RedisConfig.kt
+++ src/main/kotlin/com/google/shinyay/config/RedisConfig.kt
@@ -1,15 +1,19 @@
 package com.google.shinyay.config
 
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 import org.springframework.data.redis.serializer.GenericToStringSerializer
 
 
 
 
 @Configuration
+@ComponentScan("com.google.shinyay")
 class RedisConfig {
 
     @Bean
