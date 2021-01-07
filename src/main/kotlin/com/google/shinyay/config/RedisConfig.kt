594d7b2020540ed99a3f52b246c71e3104bec957
shinyay <shinya.com@gmail.com>
2021-01-07 12:05:07 +0900

    Modify import to optimize

--- src/main/kotlin/com/google/shinyay/config/RedisConfig.kt
+++ src/main/kotlin/com/google/shinyay/config/RedisConfig.kt
@@ -1,6 +1,5 @@
 package com.google.shinyay.config
 
-import com.fasterxml.jackson.databind.ser.std.StringSerializer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
